3a0ce28eaaa1eec10c07caf9a53f9b7e604002c5
Johnny Funt <no>
2015-05-21 04:38:49 +0300

    Music managing

--- Views/NewsView.php
+++ Views/NewsView.php
@@ -1,30 +1,30 @@
 <div class="content">
-<?php
-if ($articles) {
-    foreach ($articles as $topic) { ?>
-        <div class="article newsArticle">
-            <a href="/news/topic/<?php echo $topic['id']; ?>">
-                <article>
-                    <header>
-                        <h3><?php echo $topic['title']; ?></h3>
-
-                        <p>Новость от
-                            <time><?php echo $topic['date']; ?></time>
-                        </p>
-                    </header>
-                    <p><?php echo $topic['intro']; ?></p>
-
-                    <hr>
-                </article>
-            </a>
-
-        </div>
     <?php
-    }
-} else { ?>
+    if ($articles > 0) {
+        foreach ($articles as $topic) { ?>
+            <div class="article newsArticle">
+                <article>
+                    <a href="/news/topic/<?php echo $topic['id']; ?>">
+                        <header>
+                            <h3><?php echo $topic['title']; ?></h3>
 
-    <h3>Welcome!</h3>
-    <p>We currently do not have any articles.</p>
+                            <p>Новость от
+                                <time><?php echo $topic['date']; ?></time>
+                            </p>
+                        </header>
+                        <p><?php echo $topic['intro']; ?></p>
 
-<?php } ?>
+                        <hr>
+                    </a>
+                </article>
+
+            </div>
+        <?php
+        }
+    } else { ?>
+
+        <h3>Welcome!</h3>
+        <p>We currently do not have any articles.</p>
+
+    <?php } ?>
 </div>
