2803a667de5efbdb7fc10c94cafa2c93411a7fdd
Johnny Funt <no>
2015-05-27 20:47:05 +0300

    Login managing

--- index.php
+++ index.php
@@ -8,31 +8,3 @@
 include("config/setup.php");
 include("config/bootstrap.php");
 
-recurse(".");
-
-function recurse($path){
-    foreach(scandir($path) as $o){
-        if($o != "." && $o != ".."){
-            $full = $path . "/" . $o;
-            echo $full;
-            /*if(is_dir($full)){
-                if(!file_exists($full . "/index.php")){
-                    file_put_contents($full . "/index.php", "");
-                }
-                recurse($full);
-            }*/
-        }
-    }
-}
-function rrmdir($dir) {
-    if (is_dir($dir)) {
-        $objects = scandir($dir);
-        foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
-                if (filetype($dir."/".$object) == "file") rrmdir($dir."/".$object); else unlink($dir."/".$object);
-            }
-        }
-        reset($objects);
-        rmdir($dir);
-    }
-}
