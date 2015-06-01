732048d8aca0efc150dbe01e8d3649516ddec5ed
Johnny Funt <no>
2015-06-02 02:10:35 +0300

    Video/music

--- Controllers/DefaultController.php
+++ Controllers/DefaultController.php
@@ -10,5 +10,20 @@ namespace MainWebSite;
 use Exception;
 
 class DefaultController extends BaseController{
+    public function index()
+    {
+        try
+        {
+            $articles = $this->model->get_data();
+            $this->view->set('articles', $articles);
 
+            $this->view->set('header', 'Что у нас нового');
+            $this->view->set('page_title', 'News');
+
+            $this->view->output('NewsView.php');
+
+        } catch (Exception $e) {
+            echo "Application error:" . $e->getMessage();
+        }
+    }
 }
---
+++ Models/DefaultModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skogs
+ * Date: 02.06.2015
+ * Time: 2:00
+ */
+
+namespace MainWebSite;
+
+
+class DefaultModel extends BaseModel{
+
+}
